67614c816ec76b4a81ca8a06b5831225c3a4426e
ak1r4 <ak1r4.j@gmail.com>
2020-12-17 10:24:48 +0000

    Day13 part 1

---
+++ day13/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <chrono>
+#include <limits>
+#include <cmath>
+
+using namespace std::chrono;
+
+int main(int, char** argv) {
+    std::fstream input(argv[1]);
+    std::stringstream buf;
+    buf << input.rdbuf();
+    std::string str = buf.str();
+
+    auto t0 = high_resolution_clock::now();
+    std::string_view s = str;
+
+    auto endl = s.find('\n');
+    auto earliest_tm = std::stol(s.substr(0, endl).data());
+    s.remove_prefix(endl + 1);
+
+    int min_wait = std::numeric_limits<int>::max();
+    int min_depart = 0;
+
+    while (!s.empty()) {
+        auto delimiter = s.find(',');
+        if (delimiter == s.npos) {
+            delimiter = s.find('\n');
+        }
+        auto depart_str = s.substr(0, delimiter);
+
+        if (depart_str[0] != 'x') {
+            auto depart = std::stol(depart_str.data());
+            int wait = (std::ceil(earliest_tm / static_cast<double>(depart)) * depart) - earliest_tm;
+            if (wait < min_wait) {
+                min_wait = wait;
+                min_depart = depart;
+            }
+        }
+
+        s.remove_prefix(delimiter + 1);
+    }
+    auto part_1 = min_wait * min_depart;
+    auto t1 = high_resolution_clock::now();
+
+    std::cout << "Part 1: " << part_1
+              << "(Lapse " << duration_cast<nanoseconds>(t1 - t0).count() << " ns)\n";
+    return 0;
+}
