8cee9df15b1d3f47d187070d1d59dd9dbf965245
ak1r4 <ak1r4.j@gmail.com>
2020-12-19 08:45:21 +0000

    Day13 part 2

--- day13/main.cpp
+++ day13/main.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <limits>
 #include <cmath>
+#include <vector>
+#include <utility>
 
 using namespace std::chrono;
 
@@ -48,5 +50,37 @@ int main(int, char** argv) {
 
     std::cout << "Part 1: " << part_1
               << "(Lapse " << duration_cast<nanoseconds>(t1 - t0).count() << " ns)\n";
+
+    auto t2 = high_resolution_clock::now();
+    s = str;
+    s.remove_prefix(s.find('\n') + 1);
+    std::int64_t i = 0;
+    std::vector<std::pair<std::int64_t, std::int64_t>> departs;
+    while (!s.empty()) {
+        auto delim = s.find(',');
+        if (delim == s.npos) {
+            delim = s.find('\n');
+        }
+        auto depart_str = s.substr(0, delim);
+        if (depart_str[0] != 'x') {
+            departs.emplace_back(std::stoi(depart_str.data()), i);
+        }
+        s.remove_prefix(delim + 1);
+        ++i;
+    }
+
+    std::int64_t tm = 0;
+    std::int64_t step = 1;
+    for (const auto& [depart, offset] : departs) {
+        while ((tm + offset) % depart != 0) {
+            tm += step;
+        }
+        step *= depart;
+    }
+
+    auto t3 = high_resolution_clock::now();
+    std::cout << "Part 2: " << tm
+              << "(Lapse " << duration_cast<nanoseconds>(t3 - t2).count() << " ns)\n";
+
     return 0;
-}
+ }
