fb0f7b7995c50fa51b986b2a80fc8b8b5eb0c7d5
ak1r4 <ak1r4.j@gmail.com>
2020-12-21 14:29:14 +0000

    Day15 - optimize 10x

--- day15/main.cpp
+++ day15/main.cpp
@@ -1,46 +1,61 @@
 #include "../utils.h"
 
-#include <deque>
-#include <unordered_map>
+#include <cstring>
 
-//
-// 0, 3, 6, 0, 3, 3, 1, 0,
+int reciter(std::string_view s, std::size_t target) {
+    auto size_ = target * 3;
+    int* num_freqs_ = new int[size_];
+    int prev_num_ = -1;
+    int i_ = 0;
+    std::memset(num_freqs_, -1, size_ * sizeof(int));
+
+    while (!s.empty()) {
+        auto delim = s.find(',');
+        if (delim == s.npos) {
+            delim = s.find('\n');
+        }
+        auto n = std::stoul(s.substr(0, delim).data());
+        num_freqs_[n * 3] = 1;
+        num_freqs_[n * 3 + 1] = i_;
+        ++i_;
+        prev_num_ = n;
+        s.remove_prefix(delim + 1);
+    }
+
+    while (i_ < target) {
+        int n = 0;
+        if (num_freqs_[prev_num_ * 3 + 2] != -1) {  // not first time
+            n = (
+                num_freqs_[prev_num_ * 3 + num_freqs_[prev_num_ * 3]] -
+                num_freqs_[prev_num_ * 3 + 3 - num_freqs_[prev_num_ * 3]]
+            );
+        }
+
+        if (num_freqs_[n * 3] == -1) {  // first time
+            num_freqs_[n * 3] = 1;
+        } else {
+            num_freqs_[n * 3] = 3 - num_freqs_[n * 3];
+        }
+        num_freqs_[n * 3 + num_freqs_[n * 3]] = i_;
+        prev_num_ = n;
+        ++i_;
+    }
+
+    delete [] num_freqs_;
+    return prev_num_;
+}
+
 int main(int, char** argv) {
-    int target_idx = 2020;
-
     auto part_1 = [&](std::string_view s) {
-        std::unordered_map<int, std::deque<std::size_t>> prev_num_idxes;
-
-        std::size_t i = 0;
-        int prev_num;
-        while (!s.empty()) {
-            auto delim = s.find(',');
-            if (delim == s.npos) {
-                delim = s.find('\n');
-            }
-            auto n = std::stoi(s.substr(0, delim).data());
-            prev_num_idxes[n].push_back(i);
-            ++i;
-            prev_num = n;
-            s.remove_prefix(delim + 1);
-        }
-
-        while (i < target_idx) {
-            int num = 0;
-            if (auto it = prev_num_idxes.find(prev_num); it != prev_num_idxes.end()) {
-                num = it->second.back() - it->second.front();
-            }
-            if (prev_num_idxes[num].size() == 2) {
-                prev_num_idxes[num].pop_front();
-            }
-            prev_num_idxes[num].push_back(i);
-            ++i;
-            prev_num = num;
-        }
-        return prev_num;
+        return reciter(s, 2020);
     };
-    run(part_1, argv[1], "Part 1");
-    target_idx = 30000000;
-    run(part_1, argv[1], "Part 2");
 
+    auto part_2 = [&](std::string_view s) {
+        return reciter(s, 30000000);
+    };
+
+    run(part_1, argv[1], "Part 1");
+    run(part_2, argv[1], "Part 2");
+
+    return 0;
 }
